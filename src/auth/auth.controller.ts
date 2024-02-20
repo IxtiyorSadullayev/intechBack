c57114a7746c29d7156a9e4bf8bba9bf69ee038b
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2024-02-20 21:56:49 +0500

    login and create  user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
 
     @Get()
     getUsers(){}
+
+    @Post("register")
+    createUser(){}
+
+    @Post("login")
+    loginUser(){}
 
 }
